695d1115d5afd1f3d3a040113221a7db19a29a63
salama135 <salamaa135@gmail.com>
2022-08-05 21:51:29 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent {
   title = 'Top 3 Peronal Values';
+
+  choose: boolean;
+
+  ngOnInit() {
+    this.choose = false;
+  }
+
+  next() {
+    this.choose = true;
+  }
+
+  reset() {
+    this.choose = false;
+  }
 }
