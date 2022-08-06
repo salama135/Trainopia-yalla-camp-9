e0ee276490136f43b21f309823d8cff5cd1d78e3
salama135 <salamaa135@gmail.com>
2022-08-06 11:21:33 +0200

    8 files modified

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -2,8 +2,8 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'hello',
-  template: `<div class="jumbotron bg-primary rounded-5">
-  <h1 class="display-4">{{title}}</h1>
+  template: `<div class="jumbotron  rounded-5" style="background-color: #00ADB5;">
+  <h1 class="fs-1 fw-bolder">{{title}}</h1>
   </div>`,
   styles: [`h1 { }`],
 })
