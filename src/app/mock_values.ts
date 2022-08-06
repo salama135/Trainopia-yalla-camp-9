ab4c6656c4c7fd9f3e7a06a8a51729a58c437739
salama135 <salamaa135@gmail.com>
2022-08-06 10:42:32 +0200

    1 file modified

--- src/app/mock_values.ts
+++ src/app/mock_values.ts
@@ -4,14 +4,14 @@ export const MAX_NUMBER_OF_VALUES: number = 10;
 export const isArabic: boolean = true;
 
 export const VALUES: Value[] = [
-  { id: 1, title: 'certainty' },
-  { id: 2, title: 'control' },
-  { id: 3, title: 'security' },
-  { id: 4, title: 'peace' },
-  { id: 5, title: 'health' },
-  { id: 6, title: 'discipline' },
-  { id: 7, title: 'job security' },
-  { id: 8, title: 'waelth' },
-  { id: 9, title: 'financial' },
-  { id: 10, title: 'pleasure' },
+  // { id: 1, title: 'certainty' },
+  // { id: 2, title: 'control' },
+  // { id: 3, title: 'security' },
+  // { id: 4, title: 'peace' },
+  // { id: 5, title: 'health' },
+  // { id: 6, title: 'discipline' },
+  // { id: 7, title: 'job security' },
+  // { id: 8, title: 'waelth' },
+  // { id: 9, title: 'financial' },
+  // { id: 10, title: 'pleasure' },
 ];
